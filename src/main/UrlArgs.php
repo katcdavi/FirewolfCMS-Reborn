e3f56247aa8eb6a1754e6fe6f29bdff371ba9cd4
David Katczinsky <47692147+Katczinsky21@users.noreply.github.com>
2021-04-09 10:40:36 +0200

    finished parsing of url and get params

--- src/main/UrlArgs.php
+++ src/main/UrlArgs.php
@@ -1,33 +1,72 @@
 <?php
 
+namespace FCMS;
+
+
+use http\Exception;
 
 final class UrlArgs
 {
     /** @var string $url */
     private $url;
+    /** @var array $positional */
+    private $positional;
     /** @var array $dict */
     private $dict;
     /** @var Config $conf */
     private $conf;
+    /** @var int $pos */
+    private $pos = 0;
 
-    public function __construct(Config $conf, string $url = null)
-    {
+    public function __construct(Config $conf, string $url = null) {
         $this->url = $url ?? $_SERVER['REQUEST_URI'];
         $this->conf = $conf;
+        $this->init();
+    }
 
+    public function getNext(): ?string {
+        return $this->positional[$this->pos++] ?? null;
+    }
+
+    public function getArg(string $key): ?string {
+        return $this->dict[$key] ?? null;
+    }
+
+    private function init() {
         if ($this->conf->isSet('subdir')) {
             $this->url = str_replace($this->conf->get('subdir'), '', $this->url);
         }
 
-        var_dump($this->url);
-        echo "<br><br>";
-        $this->dict = explode("/", $this->url);
-
-        $this->dict = array_filter($this->dict, function ($e) {
-            return !empty($e);
+        $exploded = explode("/", $this->url);
+        $exploded = array_filter($exploded, function ($e) {
+            return !empty($e)|| strlen($e) > 0;
         });
-        echo "<br><br>";
-        var_dump($this->dict);
-        echo "<br><br>";
+        $exploded = array_values($exploded);
+
+        $this->dict = [];
+        if (count($exploded) > 0) {
+            $lastItem = $exploded[count($exploded) - 1];
+
+            if (strpos($lastItem, '?') !== FALSE) {
+                $paramsStr = substr($lastItem, strpos($lastItem, '?') + 1);
+                $exploded[count($exploded) - 1] = str_replace('?'.$paramsStr, '', $lastItem);
+                $this->parseParams($paramsStr);
+            }
+        }
+
+        $this->positional = $exploded;
+    }
+
+    private function parseParams(string $paramsStr) {
+        $pairs = explode("&", $paramsStr);
+
+        $this->dict = [];
+        array_map(function ($pair) {
+            $exploded = explode("=", $pair);
+            if (count($exploded) !== 2) {
+                throw new \Exception("Invalid URL parameters");
+            }
+            $this->dict[$exploded[0]] = $exploded[1];
+        }, $pairs);
     }
 }
